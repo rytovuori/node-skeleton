a8f350695e48c1cc5fe729778f8d7325276d9af5
robtot <rytovro@ixonos.local>
2018-04-06 16:09:14 +0300

    added unit test

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+var port = 80
+
+app.get('/', (req, res) => res.json({ msg: 'Hello World!' }))
+
+
+let server = app.listen(port, () => console.log('Example app listening on port '+ port +'!'))
+
+app.stopServer = function stopServer() {
+  server.close()
+}
+
+module.exports = app;
---
+++ test/unit/hello_test.js
@@ -0,0 +1,22 @@
+var chai = require('chai');
+const assert = require('assert');
+var should = chai.should();
+//var sinon = require('sinon');
+var request = require('supertest');
+var app = require('../../src/index.js');
+
+describe('GET /',() => {
+  after( () => app.stopServer())
+  it('return Hello World', (done) =>{
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function(err, res){
+        assert(!err);
+        should.equal(res.body.msg, 'Hello World!');
+        done()
+      });
+
+  });
+
+});
