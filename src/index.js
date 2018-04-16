bf586974f0f350a99972765d1e40be9f3ef85182
Rytövuori Robert <rytovro@ixonos.local>
2018-04-16 13:55:31 +0300

    changed hello back to hello world

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 var port = 80
 
-app.get('/', (req, res) => res.json({ msg: 'Hello!' }))
+app.get('/', (req, res) => res.json({ msg: 'Hello World!' }))
 
 
 let server = app.listen(port, () => console.log('Example app listening on port '+ port +'!'))
